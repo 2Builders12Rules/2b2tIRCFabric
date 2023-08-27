263621bd0eaefaaa957774ae684e4aaa67a88be7
Steve3-ai <man@tone.5>
2023-08-27 09:42:15 -0700

    Changes: - Changed version to 1.0.1 - Reverted a change that prevented server from using command prefixes other than /

--- src/main/java/org_2b12r/irc2b2t/mixin/MixinClientPlayNetworkHandler.java
+++ src/main/java/org_2b12r/irc2b2t/mixin/MixinClientPlayNetworkHandler.java
@@ -1,6 +1,5 @@
 package org_2b12r.irc2b2t.mixin;
 
-
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,16 +7,17 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org_2b12r.irc2b2t.fabric.IRC2b2t;
 
-@Mixin(value = ClientPlayNetworkHandler.class, priority = 1001)
+@Mixin(value = ClientPlayNetworkHandler.class, priority = Integer.MIN_VALUE)
 public class MixinClientPlayNetworkHandler {
     @Inject(method = "sendChatMessage", at = @At("HEAD"), cancellable = true)
-    private void onSendChatMessage(String message, CallbackInfo cir) {
-        if (IRC2b2t.onChat(message))
-            cir.cancel();
+    public void sendChatMessage(String content, CallbackInfo ci) {
+        if (IRC2b2t.onChat(content))
+            ci.cancel();
     }
+
     @Inject(method = "sendChatCommand", at = @At("HEAD"), cancellable = true)
-    private void onSendChatCommand(String command, CallbackInfo cir) {
-        if (IRC2b2t.onCommand(command))
-            cir.cancel();
+    public void sendChatCommand(String command, CallbackInfo ci) {
+        if (IRC2b2t.onChat("/" + command))
+            ci.cancel();
     }
 }
