db095a17a24fba924c0c62e997ddca2ecc07b190
Steve3-ai <man@tone.5>
2023-08-28 07:24:22 -0700

    Changes: - Removed unused methods - Fixed strings being decoded incorrectly for some users - Changed version to 1.0.3

--- src/main/java/org_2b12r/irc2b2t/net/DataBuffer.java
+++ src/main/java/org_2b12r/irc2b2t/net/DataBuffer.java
@@ -16,11 +16,6 @@ public class DataBuffer {
         this.writePos = 0;
     }
 
-    public DataBuffer(byte[] bytes) {
-        this.buffer = bytes;
-        this.writePos = bytes.length;
-    }
-
     public void writeByte(int b) {
         this.ensureCapacity(this.writePos + 1);
         this.buffer[this.writePos++] = (byte) b;
@@ -28,14 +23,6 @@ public class DataBuffer {
 
     public byte readByte() {
         return this.readPos < this.writePos ? this.buffer[this.readPos++] : 0;
-    }
-
-    public void writeUnsignedByte(int b) {
-        this.writeByte(b);
-    }
-
-    public int readUnsignedByte() {
-        return this.readByte() & 0xFF;
     }
 
     public void writeBytes(byte[] b) {
@@ -70,21 +57,11 @@ public class DataBuffer {
         return buffer;
     }
 
-    public byte[] readBytes() {
-        return this.readBytes(this.available());
-    }
-
     public void writeString(String string) {
         final byte[] bytes = string.getBytes(StandardCharsets.UTF_8);
 
         this.writeVarInt(bytes.length);
         this.writeBytes(bytes);
-    }
-
-    // TODO: Remove and replace code that uses this with writeList
-    public void writeStrings(String[] input) {
-        for (int i = 0; i < input.length; i++)
-            writeString(input[i]);
     }
 
     public String readString() {
@@ -95,15 +72,7 @@ public class DataBuffer {
         final int length = this.readVarInt();
         if (length > limit || length < 0)
             throw new PacketException("Invalid string length: %d. Max Length: %d.", length, limit);
-        return new String(this.readBytes(length));
-    }
-
-    // TODO: Remove and replace code that uses this with readList
-    public String[] readStrings(int length) {
-        final String[] strings = new String[length];
-        for (int i = 0; i < length; i++)
-            strings[i] = this.readString();
-        return strings;
+        return new String(this.readBytes(length), StandardCharsets.UTF_8);
     }
 
     public void writeShort(short input) {
@@ -156,30 +125,6 @@ public class DataBuffer {
         return longs;
     }
 
-    public void writeFloat(float input) {
-        this.writeInt(Float.floatToRawIntBits(input));
-    }
-
-    public float readFloat() {
-        return Float.intBitsToFloat(this.readInt());
-    }
-
-    public void writeDouble(double input) {
-        this.writeLong(Double.doubleToRawLongBits(input));
-    }
-
-    public double readDouble() {
-        return Double.longBitsToDouble(this.readLong());
-    }
-
-    public void writeBoolean(boolean b) {
-        this.writeByte(b ? 1 : 0);
-    }
-
-    public boolean readBoolean() {
-        return this.readByte() == 1;
-    }
-
     public void writeUUID(UUID uuid) {
         this.writeLong(uuid.getMostSignificantBits());
         this.writeLong(uuid.getLeastSignificantBits());
@@ -199,20 +144,6 @@ public class DataBuffer {
             this.writeByte(temp);
         }
         while (input != 0);
-        /*for (int i = 0; i < 5; i++) {
-            final int currentByte = input & 0b1111111;
-            final boolean hasNext = (input >>>= 7) > 0;
-
-            this.writeByte(hasNext ? 0b10000000 | currentByte : currentByte);
-
-            if (!hasNext)
-                break;
-        }*/
-    }
-
-    public void writeVarInts(int[] input) {
-        for (int i = 0; i < input.length; i++)
-            writeVarInt(input[i]);
     }
 
     public int readVarInt() {
@@ -227,62 +158,6 @@ public class DataBuffer {
         }
 
         return out;
-    }
-
-    public int[] readVarInts(int length) {
-        final int[] ints = new int[length];
-        for (int i = 0; i < length; i++)
-            ints[i] = readVarInt();
-        return ints;
-    }
-
-    public void writeVarLong(long input) {
-        do {
-            byte temp = (byte) (input & 0b01111111);
-            input >>>= 7;
-            if (input != 0) {
-                temp |= 0b10000000;
-            }
-            this.writeByte(temp);
-        }
-        while (input != 0);
-    }
-
-    public void writeVarLongs(long[] input) {
-        for (int i = 0; i < input.length; i++)
-            writeVarLong(input[i]);
-    }
-
-    public long readVarLong() {
-        long out = 0;
-        for (int i = 0; i < 10; i++) {
-            final byte read = this.readByte();
-
-            out |= ((long) (read & 0b01111111) << (7 * i)); // Add 7 bits to output VarInt
-
-            if ((read & 0b10000000) == 0) // Break if first bit is 0 (Next byte is not part of VarInt)
-                break;
-        }
-
-        return out;
-    }
-
-    public long[] readVarLongs(int length) {
-        final long[] longs = new long[length];
-        for (int i = 0; i < length; i++)
-            longs[i] = readVarLong();
-        return longs;
-    }
-
-    public void writeBitSet(BitSet bitSet) {
-        final long[] longArray = bitSet.toLongArray();
-        this.writeVarInt(longArray.length);
-        this.writeLongs(longArray);
-    }
-
-    public BitSet readBitSet() {
-        final int length = this.readVarInt();
-        return BitSet.valueOf(this.readLongs(length));
     }
 
     public <T> void writeList(List<T> input, Consumer<T> writer) {
@@ -310,10 +185,6 @@ public class DataBuffer {
         this.writeBytes(b);
     }
 
-    public byte[] readByteArray() {
-        return this.readByteArrayLimited(this.available());
-    }
-
     public byte[] readByteArrayLimited(int limit) {
         final int length = this.readVarInt();
         if (length > limit) {
@@ -323,14 +194,6 @@ public class DataBuffer {
             this.readBytes(bytes);
             return bytes;
         }
-    }
-
-    public int varIntSize(int input) {
-        return (31 - Integer.numberOfLeadingZeros(input)) / 7 + 1;
-    }
-
-    public int varLongSize(int input) {
-        return (63 - Long.numberOfLeadingZeros(input)) / 7 + 1;
     }
 
     private void ensureCapacity(int minCapacity) {
